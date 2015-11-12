7b97f878c86d03b6a203164b8796bafc43676470
Haley Bash <halebash@umich.edu>
2015-11-11 19:33:20 -0800

    Add Express server that renders index.html and can call to Twitter's API

---
+++ server/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var Twitter = require('twitter');
+var consumerKeys = require('./consumerKeys.js');
+
+var port = 3000;
+
+app.get('/', function(req, res) {
+  var pathName = ''
+  res.sendFile(path.join(__dirname + '/../client/index.html'));
+});
+
+app.get('/articles', function(req, res) {
+  //twitter api call
+});
+
+app.listen(port);
+
+var client = new Twitter({
+  consumer_key: consumerKeys.consumerKey,
+  consumer_secret: consumerKeys.consumerSecret,
+  access_token_key: '',
+  access_token_secret: ''
+});
+
+client.get('search/tweets', {q: 'kim kardashian', count: 1, result_type: 'popular', vertical: 'news'}, function(error, tweets, response) {
+  console.log(tweets.statuses);
+});
+
+module.exports = app;
