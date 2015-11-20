fe1ef2be374bbd99427762c67804125ebe7705d1
Jessica O'Brien <j.e.obrien28@gmail.com>
2015-11-19 20:31:07 -0800

    Moved api ID

--- server/server-config.js
+++ server/server-config.js
@@ -4,6 +4,7 @@ var app = express();
 var Query = require('../db/query');
 var bodyParser = require('body-parser');
 var alerts = require('../db/TravelAlerts.json');
+var just_giving = require('./keys');
 
 app.use(bodyParser.json());
 
@@ -17,7 +18,7 @@ app.get('/warnings', function(req, res) {
   });
 });
 
-// on get to /issues, get issues for a country
+
 app.get('/issues', function(req, res) {
   // Need from frontend: query country, begin_date
   var url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=france&fq=section_name:("Front Page" "Global Home" "International Home" "NYT Now" "Today\'s Headlines" "Topics" "World")&begin_date=20151119&api-key=7c797d4528b7af8fdc9e050d1e22dfa0:16:73530952';
@@ -41,14 +42,14 @@ app.get('/issues', function(req, res) {
 // app get charities for keyword (keyword should be the country name)
 app.get('/charities', function(req, res) {
   var options = {
-    url: 'https://api.justgiving.com/{8a8d1f89}/v1/onesearch?q={"Syria"}',
+    url: 'https://api.justgiving.com/{'+just_giving+'}/v1/onesearch?q={"'+req.params.country+'"}',
     headers: {
       'Accept': 'application/json'
     }
   };
   request(options, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      // res.send(response.body);
+      res.send(response.body);
     }
   });
 });
