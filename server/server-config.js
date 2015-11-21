359dad5604c67f3ba77cefbec9781a32bf44ef10
Jessica O'Brien <j.e.obrien28@gmail.com>
2015-11-20 17:13:07 -0800

    Resolved merge conflicts in server file

--- server/server-config.js
+++ server/server-config.js
@@ -18,31 +18,31 @@ app.get('/warnings', function(req, res) {
 });
 
 
-app.get('/issues', function(req, res) {
+app.get('/issues', function (req, res) {
   // DATE FORMATTING =================================================
   var today = new Date(), day = today.getDate(), month = today.getMonth()+1, year = today.getFullYear();
   month = (month < 10 ? "0" : "") + month;
   day = (day < 10 ? "0" : "") + day;
   todaysDate = ""+year+month+day;
+
   // BUILD QUERY URLS ================================================
   var ny_url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+req.query.country+'&fq=section_name:("Front Page" "Global Home" "International Home" "NYT Now" "Today\'s Headlines" "Topics" "World")&begin_date='+todaysDate+'&api-key='+keys.ny_times;
   var giving_url = 'https://api.justgiving.com/{'+keys.just_giving+'}/v1/onesearch?q={'+req.query.country+'}';
-  // SET HEADERS =====================================================
- 
+
   // API REQUESTS ====================================================
   var results = {};
-  request(ny_url, function(error, response, body) {
+  request(ny_url, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       body = JSON.parse(body);
       var newsArray = [];
       var articleArray = body.response.docs;
-      for (var i = 0; i < articleArray.length; i++) {
+      articleArray.forEach(function (article) {
         var obj = {
-          headline: articleArray[i].headline.main,
-          url: articleArray[i].web_url
+          headline: article.headline.main,
+          url: article.web_url
         };
         newsArray.push(obj);
-      }
+      });
       results.news = newsArray;
       var options = {
         url: giving_url,
@@ -50,10 +50,9 @@ app.get('/issues', function(req, res) {
           'Accept': 'application/json'
         }
       };
-      request(options, function(error, response, body) {
+      request(options, function (error, response, body) {
         if (!error && response.statusCode === 200) {
           var body = JSON.parse(response.body);
-          // console.log(response.body["GroupedResults"]);
           var indexes = body["GroupedResults"];
           indexes.forEach(function (index) {
             if (index["Title"] === "Charities") {
@@ -66,21 +65,5 @@ app.get('/issues', function(req, res) {
     }
   });
 });
-
-// return an object for the country that looks like {news: [], charities: []}
-
-// app.get('/charities', function(req, res) {
-//   var options = {
-//     url: 'https://api.justgiving.com/{'+keys.just_giving+'}/v1/onesearch?q={'+req.query.country+'}',
-//     headers: {
-//       'Accept': 'application/json'
-//     }
-//   };
-//   request(options, function(error, response, body) {
-//     if (!error && response.statusCode === 200) {
-//       res.send(response.body);
-//     }
-//   });
-// });
 
 module.exports = app;
