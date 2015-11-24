7d8cf7a71107e9e559b8f140530beae7f3696209
Lorenzo De Nobili <elledienne@gmail.com>
2015-11-23 16:06:52 -0800

    Moved zoom center a little bit on the right to increase space for the tooltip

--- client/src/views/MapView.js
+++ client/src/views/MapView.js
@@ -90,7 +90,8 @@ var MapView = Backbone.View.extend({
       var w_scale = (bounds[1][0] - bounds[0][0]) / width;
       var h_scale = (bounds[1][1] - bounds[0][1]) / height;
       var z = .96 / Math.max(w_scale, h_scale);
-      var x = (bounds[1][0] + bounds[0][0]) / 2;
+      // moved center zoom point to the right to make room for a bigger tooltip
+      var x = (bounds[1][0] + bounds[0][0]) / 2 - 15;
       var y = (bounds[1][1] + bounds[0][1]) / 2 + (height / z / 6);
       return [x, y, z];
     }
