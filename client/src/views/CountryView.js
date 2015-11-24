a5c1388569e6151f3b9230c73c62a27ca8c2781c
Laura Gelston <lauragelston@gmail.com>
2015-11-24 11:19:48 -0800

    Manually merged last PR

--- client/src/views/CountryView.js
+++ client/src/views/CountryView.js
@@ -1,192 +1,200 @@
 var CountryView = Backbone.View.extend({
-  events: {
-    'click': 'countryClicked',
-    'mouseenter': 'showName',
-    'mouseout': 'hideName'
-  },
-  initialize: function() {
-    var context = this;
+      events: {
+        'click': 'countryClicked',
+        'mouseenter': 'showName',
+        'mouseout': 'hideName'
+      },
+      initialize: function() {
+        var context = this;
 
-    d3.select(this.el)
-      .attr("id", function(d) {
-        context.countryID = d.id.replace(/ /g, '_')
-        return context.countryID;
-      });
+        d3.select(this.el)
+          .attr("id", function(d) {
+            context.countryID = d.id.replace(/ /g, '_')
+            return context.countryID;
+          });
 
-    this.selected = false;
-    this.selection = d3.select('svg').append('g').attr("class", context.countryID);
-
-
-    this.makeToolTip();
-    this.makeHoverTip();
-
-    this.model.on('dataLoaded', this.showCountryData, this);
-    this.model.on('selection', this.selectCountry, this);
-    this.model.on('deselection', this.deselectCountry, this);
-  },
-
-  makeToolTip: function() {
-    var context = this;
-    this.toolTip = this.selection.append('foreignObject')
-      .attr('class', 'tooltip')
-      .style('display', 'none')
-      .each(function() {
-        return context.positionToCountryCoods(this);
-      });
-    this.showCountryData();
-  },
-
-  makeHoverTip: function() {
-    var context = this;
-    var hoverTipHtml = '' +
-      '<div class="hovertip-container">' +
-      '  <div class ="bottom-aligner"></div>' +
-      '  <div class="hovertip-content">' + this.model.get('countryName') + '</div>' +
-      '</div>';
-
-    this.hoverTip = this.selection.append('foreignObject')
-      .attr('class', 'hoverTip')
-      .style({
-        display: 'none',
-        width: 100,
-        height: 100
-      })
-      .each(function() {
-        return context.positionToMouse(this);
-      })
-      .html(hoverTipHtml)
-
-  },
-
-  positionToMouse: function(element) {
-    var context = this;
-    d3.select('svg').on('mousemove.' + context.countryID, function() {
-      d3.select(element)
-        .attr({
-          x: d3.mouse(this)[0] - 102,
-          y: d3.mouse(this)[1] - 102
-        })
-    });
-  },
+        this.selected = false;
+        this.selection = d3.select('svg').append('g').attr("class", context.countryID);
 
 
-  positionToCountryCoods: function(element) {
-    var context = this;
-    return d3.select(element)
-      .attr('x', function() {
-        var bbox = d3.select(context.el).node().getBBox();
-        return bbox.x + bbox.width / 2
-      })
-      .attr('y', function() {
-        var bbox = d3.select(context.el).node().getBBox();
-        return bbox.y + bbox.height / 2
-      });
-  },
+        this.makeToolTip();
+        this.makeHoverTip();
 
-  countryClicked: function() {
-    this.trigger('countryClicked', this);
-    this.model.get('selected') ? this.model.trigger('deselection') : this.model.trigger('selection');
-  },
+        this.model.on('dataLoaded', this.showCountryData, this);
+        this.model.on('selection', this.selectCountry, this);
+        this.model.on('deselection', this.deselectCountry, this);
+      },
 
-  selectCountry: function() {
-    this.hideName();
-    d3.select(this.el)
-      .classed('selected', true);
+      makeToolTip: function() {
+        var context = this;
+        this.toolTip = this.selection.append('foreignObject')
+          .attr('class', 'tooltip')
+          .style('display', 'none')
+          .each(function() {
+            return context.positionToCountryCoods(this);
+          });
+        this.showCountryData();
+      },
 
-    this.toolTip
-      .style('display', 'inherit')
-      .transition()
-      .duration(750)
-      .attr({
-        'x': 40,
-        'y': 50,
-        width: 300,
-        height: 300
-      })
-    if (this.model.get('issues') === undefined) {
-      console.log('getting data');
-      this.model.getData();
-    } else {
-      this.showCountryData();
-    }
-  },
+      makeHoverTip: function() {
+        var context = this;
+        var hoverTipHtml = '' +
+          '<div class="hovertip-container">' +
+          '  <div class ="bottom-aligner"></div>' +
+          '  <div class="hovertip-content">' + this.model.get('countryName') + '</div>' +
+          '</div>';
 
-  deselectCountry: function() {
-    this.toolTip
-      .style('display', 'none');
-  },
+        this.hoverTip = this.selection.append('foreignObject')
+          .attr('class', 'hoverTip')
+          .style({
+            display: 'none',
+            width: 100,
+            height: 100
+          })
+          .each(function() {
+            return context.positionToMouse(this);
+          })
+          .html(hoverTipHtml)
 
-  showCountryData: function() {
-    var context = this;
-    var news = this.model.get('news') || [];
-    var charities = this.model.get('charities') || [];
-    //console.log(this)
-    var html = [];
-    html[0] = '' +
-      '<div class="tooltip-container">' +
-      '  <div class="flag" style="background-image: url(\'/src/img/Libya.png\')"></div>' +
-      '  <div class="tooltip-title">' +
-      '    <p class="tooltip-title-text">' + context.model.get('countryName') + '</p>' +
-      '  </div>' +
-      '  <div class="tooltip-article">' +
-      '    <div class="tooltip-article-header">What\'s going on rigth now</div>' +
-      '    <hr>' +
-      '    <div class="tooltip-article-content">' +
-      '      <ul>';
-    html[1] = [];
-    html[2] = '' +
-      '      </ul>' +
-      '    </div>' +
-      '  </div>' +
-      '  <div>' +
-      '    <div class="tooltip-charity-header">Maybe something here</div>' +
-      //'    <hr>' +
-      '    <div class="tooltip-charity-content">' +
-      '      <ul>';
-    html[3] = [];
-    html[4] = '' +
-      '      </ul>' +
-      '    </div>' +
-      '  </div>' +
-      '</div>';
+      },
 
-    news.forEach(function(article) {
-      html[1].push('<li><a href="' + article.url + '">' + article.headline + '</a></li>');
-    });
-    charities.forEach(function(charity) {
-      console.log(charity)
-      var charHtml = '' +
-        '<li class="tooltip-charity-box">' +
-        '  <div src="' + charity.Logo + '"></div>' +
-        '  <a href="' + charity.Link + '">' + charity.Name + '</a>' +
-        '  <div class="charity-description">' + charity.Description + '</div>' +
-        '</li>';
+      positionToMouse: function(element) {
+        var context = this;
+        d3.select('svg').on('mousemove.' + context.countryID, function() {
+          d3.select(element)
+            .attr({
+              x: d3.mouse(this)[0] - 102,
+              y: d3.mouse(this)[1] - 102
+            })
+        });
+      },
 
-      html[3].push(charHtml);
-    });
 
-    this.toolTip
-      .html(this.htmlBuilder(html))
+      positionToCountryCoods: function(element) {
+        var context = this;
+        return d3.select(element)
+          .attr('x', function() {
+            return (context.model.get('x') * 2) / 3;
+          })
+          .attr('y', function() {
+            var bbox = d3.select(context.el).node().getBBox();
+            return bbox.y + bbox.height / 2
+          });
+      },
 
-  },
+      countryClicked: function() {
+        this.trigger('countryClicked', this);
+        this.model.get('selected') ? this.model.trigger('deselection') : this.model.trigger('selection');
+      },
 
-  htmlBuilder: function(html) {
-    var a = _.reduce(html, function(string, next) {
-      var toJoin = Array.isArray(next) ? next.splice(0, 5).join('') : next
-      return string + toJoin;
-    }, '');
-    //console.log(a);
-    return a;
-  },
+      selectCountry: function() {
+        this.hideName();
+        d3.select(this.el)
+          .classed('selected', true);
 
-  showName: function() {
-    if (!this.model.get('selected')) {
-      this.hoverTip.style('display', 'inherit');
-    }
-  },
+        this.toolTip
+          .style('display', 'inherit')
+          .transition()
+          .duration(750)
+          .attr({
+            'x': 20,
+            'y': 50,
+            width: 300,
+            height: 300
+          })
+        if (this.model.get('issues') === undefined) {
+          console.log('getting data');
+          this.model.getData();
+        } else {
+          this.showCountryData();
+        }
+      },
 
-  hideName: function() {
-    this.hoverTip.style('display', 'none');
-  }
+      deselectCountry: function() {
+        this.toolTip
+          .style('display', 'none');
+      },
 
-});
+      showCountryData: function() {
+        var context = this;
+        var news = this.model.get('news') || [];
+        var charities = this.model.get('charities') || [];
+        var flag = this.model.get('flag') || '';
+
+        flagCSS = flag !== '' ? 'background-image: url(\'/flags/' + flag + '\')' : '';
+        var html = [];
+        html[0] = '' +
+          '<div class="tooltip-container">' +
+          '  <div class="flag" style="' + flagCSS + '"></div>' +
+          '  <div class="tooltip-title">' +
+          '    <p class="tooltip-title-text">' + context.model.get('countryName') + '</p>' +
+          '  </div>' +
+          '  <div class="tooltip-article">' +
+          '    <div class="tooltip-article-header">What\'s going on right now</div>' +
+          '    <hr>' +
+          '    <div class="tooltip-article-content">' +
+          '      <ul>';
+        html[1] = [];
+        html[2] = '' +
+          '      </ul>' +
+          '    </div>' +
+          '  </div>' +
+          '  <div>' +
+          '    <div class="tooltip-charity-header">Make the difference</div>' +
+          //'    <hr>' +
+          '    <div class="tooltip-charity-content">' +
+          '      <ul>';
+        html[3] = [];
+        html[4] = '' +
+          '      </ul>' +
+          '    </div>' +
+          '  </div>' +
+          '</div>';
+
+        news.forEach(function(article, i) {
+          // this line is needed because (i don't know why) but some articles were repeated
+          if (i === 0 || article.headline !== news[i - 1].headline) {
+            html[1].push('<li><a href="' + article.url + '">' + article.headline + '</a></li>');
+          }
+        }); << << << < HEAD
+        charities.forEach(function(charity) {
+            console.log(charity) === === =
+              charities.forEach(function(charity, i) {
+                console.log(charity)
+                var logo = i === 0 ? '<div class="tooltip-charity-logo" style="background-image: url(' + charity.Logo + ')"></div>' : ''; >>> >>> > Manually merged last PR
+                var charHtml = '' +
+                  '<li class="tooltip-charity-box">' +
+                  logo +
+                  '  <a class="tooltip-charity-title" href="' + charity.Link + '">' + charity.Name.trim() + '</a>' +
+                  '  <div class="charity-description">' + charity.Description + '</div>' +
+                  '  <div class="charity-more-link"><a href="' + charity.Link + '">Help them >></a></div>' +
+                  '</li>';
+
+                html[3].push(charHtml);
+              });
+
+            this.toolTip
+              .html(this.htmlBuilder(html))
+
+          },
+
+          htmlBuilder: function(html) {
+            var a = _.reduce(html, function(string, next) {
+              var toJoin = Array.isArray(next) ? next.splice(0, 4).join('') : next
+              return string + toJoin;
+            }, '');
+            //console.log(a);
+            return a;
+          },
+
+          showName: function() {
+            if (!this.model.get('selected')) {
+              this.hoverTip.style('display', 'inherit');
+            }
+          },
+
+          hideName: function() {
+            this.hoverTip.style('display', 'none');
+          }
+
+        });
