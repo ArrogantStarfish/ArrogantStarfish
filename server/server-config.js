b4d41ad6a2a5e16e4e3b99f11a64f7595dcc69c9
Abel <ablwang@ucdavis.edu>
2015-11-30 11:46:34 -0800

    Backend documentation; retool location returns

--- server/server-config.js
+++ server/server-config.js
@@ -1,17 +1,19 @@
 // MODULES ====================================
-// Express
+// Express & utility libraries 
 var express = require('express');
 var request = require('request-promise');
 var Q = require('q'); 
 var _ = require('lodash'); 
-var app = express();
+var bodyParser = require('body-parser');
 // To initalize Mongo connection
 var db = require('../db/config.js');
 var Query = require('../db/query');
-var bodyParser = require('body-parser');
-var alerts = require('../db/TravelAlerts.json');
+// To provide API keys 
 var keys = require('./keys');
-// Reddit
+// To provide country data
+var CountryData = require('../db/CountryData');
+var alerts = require('../db/TravelAlerts.json');
+// To initialize Reddit API wrapper for simpler querying 
 var Snoocore = require('snoocore'); 
 var reddit = new Snoocore({
   userAgent: '/u/wecare_app WeCare@1.0.0', // unique string identifying the app
@@ -23,7 +25,8 @@ var reddit = new Snoocore({
     deviceId: 'DO_NOT_TRACK_THIS_DEVICE'
   }
 });
-var CountryData = require('../db/CountryData');
+// BEGIN CONFIG CODE ================================================
+var app = express();
 
 app.use(bodyParser.json());
 
@@ -39,40 +42,6 @@ app.get('/warnings', function(req, res) {
     res.json(warnings);
   });
 });
-
-// // Return /r/worldnews subreddit Top Posts
-// app.get('/topreddit', function(req, res) {
-
-//   reddit('/r/worldnews/top').listing({
-//     limit: 10
-//   })
-//     .then(function(slice) {
-//       var articleArray = slice.children; 
-//       var newsArray = articleArray.map(function(article) {
-//         // Extract country name from article headline
-//         var splitTitle = article.data.title.split(' ');
-//         var location = []; 
-//         splitTitle.forEach(function(word) {
-//           var country = _.find(CountryData, function(co) {
-//             // Example: Russian is a demonym for Russia
-//             if (co.demonym === word || co.name.common === word) {
-//               return true; 
-//             }    
-//           }); 
-//           if (country && location.length < 1) { location.push(country.name.common) }; 
-//         }); 
-//         return {
-//           headline: article.data.title,
-//           url: article.data.url,
-//           location: location
-//         }
-//       })
-//       res.send(newsArray); 
-//     })
-//     .catch(function(error) {
-//       res.send(error); 
-//     });
-// }); 
 
 // Return NYT Top News
 app.get('/top', function(req, res) {
@@ -102,11 +71,21 @@ app.get('/top', function(req, res) {
         body = JSON.parse(body);
         var articleArray = body.results.slice(0, 10);
         return articleArray.map(function(article) {
+          var location = []; 
+          article.geo_facet.forEach(function(location) {
+            var country = _.find(CountryData, function(co) {
+              // Filters for capital cities or country name 
+              if (co.capital === word || co.name.common === location) {
+                return true; 
+              }
+            }); 
+            if (country && location.length < 1) { location.push(country.name.common) }; 
+          })
           return {
             source: "NYT", 
             headline: article.title,
             url: article.url,
-            location: article.geo_facet.splice(0, 1); 
+            location: location
           };
         });
       })
@@ -128,7 +107,7 @@ app.get('/top', function(req, res) {
           var location = []; 
           splitTitle.forEach(function(word) {
             var country = _.find(CountryData, function(co) {
-              // Example: Russian is a demonym for Russia
+              // Filters for denonym or country name. Denonym example: Russian for Russia, Italian for Italy
               if (co.demonym === word || co.name.common === word) {
                 return true; 
               }    
