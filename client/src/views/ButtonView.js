ce2d179a50bb7abd0b9813aac7966bcbc480923f
Lorenzo De Nobili <elledienne@gmail.com>
2015-11-29 19:46:59 -0800

    Implement button shrink

--- client/src/views/ButtonView.js
+++ client/src/views/ButtonView.js
@@ -3,55 +3,55 @@ var Button = Backbone.View.extend({
     'click': 'buttonClicked'
   },
   initialize: function() {
-    //console.log(this.el);
     this.newsDisabled = false;
     this.isShrank = false;
     this.$mainBtn = this.$el;
     this.$mapBtn;
+    this.toggleIconClass();
   },
-  buttonClicked: function() {
-    console.log('here');
+  toggleIconClass: function() {
     if (!this.newsDisabled) {
-      this.newsDisabled = true;
-      if (this.isShrank) {
-        this.$el.addClass('ion-ios-eye');
-        this.$el.removeClass('ion-ios-eye-outline');
-      } else {
-        this.$el.text('Show Breaking News')
-      }
-      this.trigger('hideNews');
-    } else {
-      this.newsDisabled = false;
+      this.$mainBtn.text('Hide Breaking News');
       if (this.isShrank) {
         this.$el.addClass('ion-ios-eye-outline');
         this.$el.removeClass('ion-ios-eye');
-      } else {
-      this.$el.text('Hide Breaking News');
       }
-      this.trigger('showNews');
+    } else {
+      this.$mainBtn.text('Show Breaking News');
+      if (this.isShrank) {
+        this.$el.removeClass('ion-ios-eye-outline');
+        this.$el.addClass('ion-ios-eye');
+      }
     }
   },
+  buttonClicked: function() {
+    if (!this.newsDisabled) {
+      this.newsDisabled = true;
+      this.toggleIconClass();
+      this.trigger('hideNews');
+    } else {
+      this.newsDisabled = false;
+      this.toggleIconClass();
+      this.trigger('showNews');
+    }
+  }, 
   swapDomButton: function(mustShrink) {
     var newElement;
     this.$mapBtn = this.$mapBtn || $('.map-breaking-news button');
     if (!mustShrink) {
-      // attach view to 'main' button
-      //console.log('NOT SHRANK');
       newElement = this.$mainBtn;
       this.isShrank = false;
     } else {
-      // attach view to SVG button
-      //console.log('SHRANK');
       newElement = this.$mapBtn;
       this.isShrank = true;
       $('.map-bn-container').attr('x', $('svg').attr('width') - 55);
     }
-    //this.$el.replaceWith(newElement)
     this.setElement(newElement);
     this.toggleButtons();
   },
   toggleButtons: function() {
     if (this.isShrank) {
+      this.toggleIconClass();
       this.$mainBtn.css('display', 'none');
       this.$mapBtn.css('display', 'block');
     } else {
